79e3a11725dbb512a90c7581d9ec59815d105523
unknown <michaelmolina679@gmail.com>
2023-07-11 17:58:23 -0600

    starting SearchBar and CustomFilters

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,13 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
 
 export * from './Hero';
 export * from './CustomButton';
 export {
     Navbar,
-    Footer
+    Footer,
+    CustomFilter,
+    SearchBar
 }
