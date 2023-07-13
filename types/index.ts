82247a7330766df834f3277a2ef677f8076153e6
unknown <michaelmolina679@gmail.com>
2023-07-12 19:59:24 -0600

    <CarCard />

--- types/index.ts
+++ types/index.ts
@@ -16,6 +16,17 @@ export interface SearchManufacturerProps {
     setManufacturer: (manufacturer: string) => void;
 }
 
-export interface CarCardProps {
-    car: object;
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
 }
